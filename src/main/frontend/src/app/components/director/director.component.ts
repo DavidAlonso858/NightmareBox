db98fe1c2a27dabd3ce190373bc9da4b319e2a56
david <dalogar0801@g.educaand.es>
2025-05-31 19:33:21 +0200

    inicio componente directores

--- src/main/frontend/src/app/components/director/director.component.ts
+++ src/main/frontend/src/app/components/director/director.component.ts
@@ -1,18 +1,48 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Director } from '../../models/director';
+import { DirectorService } from '../../service/director.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Pelicula } from '../../models/pelicula';
+import { PeliculaService } from '../../service/pelicula.service';
 
 @Component({
   selector: 'app-director',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './director.component.html',
   styleUrl: './director.component.css'
 })
 
 export class DirectorComponent {
 
-  constructor(private title: Title) {
+  directores: Director[] = [];
+  peliculasDirector: Pelicula[] = [];
+  peliculas: Pelicula[] = [];
+
+  constructor(private title: Title, private directorService: DirectorService, private peliculaService: PeliculaService) {
     this.title.setTitle('NightmareBox - Directores');
   }
-  
+
+  ngOnInit(): void {
+    this.directorService.getDirectores().subscribe(directores => {
+      this.directores = directores;
+    });
+    this.peliculaService.getPeliculas().subscribe(peliculas => {
+      this.peliculas = peliculas;
+    });
+  }
+
+  listaPeliculasDirector(id: number) {
+    const director = this.directores.find(d => d.id === id);
+    console.log(director);
+
+    if (director) {
+      this.peliculasDirector = this.peliculas.filter(p => p.director?.id === director.id);
+    }
+    
+    return this.peliculasDirector;
+  }
+
 }
